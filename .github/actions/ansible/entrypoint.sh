d4dafb11819e383d381275e98f860754f2c6a103
VanilaMao <zmaohua@umich.edu>
2020-04-12 15:38:32 -0400

    fix ansible inventory path error

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -4,4 +4,4 @@ echo "$VAULT_PASS" > ~/.vault_password.txt
 mkdir ~/secret
 ansible-vault view --vault-password-file ~/.vault_password.txt .ansible/ssh.txt > ~/secret/id_rsa
 chmod 0600 ~/secret/id_rsa
-ansible-playbook --vault-password-file ~/.vault_password.txt -i hosts .ansible/deploy.yml
+ansible-playbook --vault-password-file ~/.vault_password.txt -i .ansible/hosts .ansible/deploy.yml
