bb7961713c87be01f461fd76b46cbd12c0c3b435
VanilaMao <zmaohua@umich.edu>
2020-04-15 15:32:42 -0400

    initalize auth service and resource server

--- offer-service/src/main/java/com/shopping/offerservice/controllers/OfferController.java
+++ offer-service/src/main/java/com/shopping/offerservice/controllers/OfferController.java
@@ -1,5 +1,11 @@
 package com.shopping.offerservice.controllers;
 
+
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OfferController {
 
     @GetMapping("/test")
-    public String test(){
-        return "hello world!";
+    public String test(Authentication auth){
+        return "hello world!" + auth.getPrincipal();
     }
 }
